c8bf3983276069d3241378633202df3e353e7213
Akif Yavuzsoy <78024188+akifyavuzsoy@users.noreply.github.com>
2024-03-02 20:55:57 +0300

    first commit in main branch

--- app/modelutil.py
+++ app/modelutil.py
@@ -1,9 +1,9 @@
 
 import os
-
-from tensorflow.keras.models import Sequential 
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv3D, LSTM, Dense, Dropout, Bidirectional, MaxPool3D, Activation, Reshape, SpatialDropout3D, BatchNormalization, TimeDistributed, Flatten
 
 def load_model() -> Sequential:
 
     model = Sequential()
+    model.add(Conv3D(128, 3, input_shape=(75, 46, 140, 1), padding='same'))
