b43dbc3828c366348980896551a706cb118be959
Akif Yavuzsoy <78024188+akifyavuzsoy@users.noreply.github.com>
2024-03-02 20:35:08 +0300

    first commit -creat project

---
+++ app/modelutil.py
@@ -0,0 +1,9 @@
+
+import os
+
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.layers import Conv3D, LSTM, Dense, Dropout, Bidirectional, MaxPool3D, Activation, Reshape, SpatialDropout3D, BatchNormalization, TimeDistributed, Flatten
+
+def load_model() -> Sequential:
+
+    model = Sequential()
